f80c0751e7594eb2d0de43e33c80d11a3cad6a7b
nadyaPeeps <nadezhda.fed.28@gmail.com>
2016-03-13 13:04:32 -0400

    css for mobile screen

--- js/controllers/TextileCtrl.js
+++ js/controllers/TextileCtrl.js
@@ -3,4 +3,19 @@ angular.module('alice').controller('TextileCtrl', TextileCtrl);
 function TextileCtrl($routeParams, $location) {
 	this.$location = $location;
 	this.currentPath = $location.path();
+	this.works = TEXTILE_DATA;
+}
+
+TextileCtrl.prototype.getWorks = function() {
+	return this.works;
+
+};
+
+// TextileCtrl.prototype.getWork = function(id) {
+// 	return this.works.filter(function(work) {return work.ID === id}) [0];
+// }
+
+var TEXTILE_DATA = {
+	"Title": "Textile Works",
+	"Images": ["images/album_cover_riso.jpg", "images/album_cover_riso.jpg", "images/album_cover_riso.jpg"]
 }
